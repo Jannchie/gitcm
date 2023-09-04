718bd3e4fbda2634b2c3df55f88d12ea23e64271
Jannchie <jannchie@gmail.com>
2023-09-04 21:21:38 +0900

    :tada: init: an opinionated git commit messages helper

---
+++ README.md
@@ -0,0 +1,163 @@
+# gitcm
+
+`gitcm` is an opinionated git commits message helper.
+
+## Installation
+
+```bash
+npm install -g gitcm
+```
+
+## Usage
+
+For example, you can use the following command:
+
+```bash
+gitcm init "init commit"
+```
+
+Which will execute the following command:
+
+```bash
+git commit -m ":tada: init: init commit"
+```
+
+## More Examples
+
+We provide an interactive cli tool.
+
+```bash
+$ gitcm
+✔ What is the commit type? (required)
+feat
+
+✔ What is the commit scope? (optional)
+Enter commit scope
+
+✔ What is the commit for? (required)
+test
+
+✔ Are you sure to execute this command? (git commit -m ":sparkles: feat: test")
+Yes
+```
+
+You can also set the type, scope and body with command-line arguments:
+
+```bash
+# git commit -m ":sparkles: feat(test): test commit"
+$ gitcm feat test "test commit" 
+
+# git commit -m ":sparkles: feat: test commit"
+$ gitcm feat "test commit" 
+```
+
+You can find help with the following command:
+
+```bash
+gitcm --help
+```
+
+## Configuration
+
+When you use this command for the first time, it writes the configuration in `~/.config/gitcm/config.json`.
+
+The default configuration is as follows:
+
+```json
+{
+  "showIcon": true,
+  "verbose": false,
+  "data": {
+    "feat": {
+      "display": "feat",
+      "emoji": ":sparkles:"
+    },
+    "chore": {
+      "display": "chore",
+      "emoji": ":wrench:"
+    },
+    "refactor": {
+      "display": "refactor",
+      "emoji": ":hammer:"
+    },
+    "fix": {
+      "display": "fix",
+      "emoji": ":bug:"
+    },
+    "hotfix": {
+      "display": "hotfix",
+      "emoji": ":ambulance:"
+    },
+    "docs": {
+      "display": "docs",
+      "emoji": ":books:"
+    },
+    "i18n": {
+      "display": "i18n",
+      "emoji": ":globe_with_meridians:"
+    },
+    "build": {
+      "display": "build",
+      "emoji": ":building_construction:"
+    },
+    "version": {
+      "display": "version",
+      "emoji": ":bookmark:"
+    },
+    "test": {
+      "display": "test",
+      "emoji": ":rotating_light:"
+    },
+    "ci": {
+      "display": "ci",
+      "emoji": ":construction_worker:"
+    },
+    "cd": {
+      "display": "cd",
+      "emoji": ":construction_worker:"
+    },
+    "workflow": {
+      "display": "workflow",
+      "emoji": ":construction_worker:"
+    },
+    "wip": {
+      "display": "wip",
+      "emoji": ":construction_worker:"
+    },
+    "init": {
+      "display": "init",
+      "emoji": ":tada:"
+    },
+    "style": {
+      "display": "style",
+      "emoji": ":art:"
+    },
+    "docker": {
+      "display": "docker",
+      "emoji": ":whale:"
+    },
+    "revert": {
+      "display": "revert",
+      "emoji": ":rewind:"
+    },
+    "config": {
+      "display": "config",
+      "emoji": ":wrench:"
+    },
+    "release": {
+      "display": "release",
+      "emoji": ":bookmark:"
+    },
+    "tag": {
+      "display": "tag",
+      "emoji": ":bookmark:"
+    }
+  }
+}
+```
+
+You can change the configuration by editing this file.
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,126 @@
+#!/usr/bin/env node
+import fs from 'node:fs'
+import path from 'node:path'
+import process from 'node:process'
+import { exec } from 'node:child_process'
+import { program } from 'commander'
+import { consola } from 'consola'
+
+// make config file if not exists in ~/.config/gitcm/config.json
+// if exists, read config file
+// if not exists, make config file
+import defaultConfig from './config.json'
+
+program
+  .name('gitcm')
+  .usage('[type] [scope] [body]')
+  .description('An opinionated git commit messages helper.')
+  .option('-d, --dry-run', 'Dry run mode')
+  .option('-y, --yes', 'Skip prompt')
+  .option('-no-i, --no-icon', 'Do not show icon')
+  .version('v0.1.0', '-V, --version', 'Output the current version')
+  .arguments('[type] [scope] [body]')
+
+program.parse()
+
+let config = defaultConfig
+
+const configPath = path.join(process.env.HOME ?? '~/', '.config/gitcm/config.json')
+if (!fs.existsSync(configPath)) {
+  const dir = path.dirname(configPath)
+  fs.mkdirSync(dir, { recursive: true })
+  consola.info(`Config file not found. Create config file in ${configPath}`)
+  fs.writeFileSync(configPath, JSON.stringify(config, null, 2))
+}
+else {
+  config = JSON.parse(fs.readFileSync(configPath, 'utf-8'))
+}
+
+const options = program.opts()
+config.showIcon = !options.noIcon
+config.verbose = options.verbose
+
+const args = program.args
+
+let type = ''
+let scope = ''
+let body = ''
+
+if (args.length > 0) {
+  type = args[0]
+  if (args.length === 2) {
+    body = args[1]
+  }
+  else if (args.length === 3) {
+    scope = args[1]
+    body = args[2]
+  }
+}
+const typeList = Object.keys(config.data)
+
+function isType(key: string): key is keyof typeof config.data {
+  return key in config.data
+}
+
+function getCMD({ type, scope, body, icon }: { type: string; body: string; scope?: string; icon?: string }) {
+  if (!scope || scope === '')
+    return `git commit -m "${`${icon} `}${type}: ${body}"`
+  return `git commit -m "${`${icon} `}${type}(${scope}): ${body}"`
+}
+
+async function waitPrompt() {
+  if (!options.yes)
+    await fillEmpty()
+
+  await confirm()
+}
+
+async function confirm() {
+  if (!isType(type)) {
+    consola.error(`Invalid commit type: ${type}`, `Commit type must be one of ${typeList.join(', ')}`)
+    process.exit(1)
+  }
+  const cmd = getCMD({ type: config.data[type].display, scope, body, icon: config.showIcon ? config.data[type].emoji : '' })
+  const answer = options.yes ? true : await consola.prompt(`Are you sure to execute this command? (${cmd})`, { type: 'confirm', initial: true })
+  if (answer) {
+    if (options.dryRun) {
+      consola.info(`Dry run: ${cmd}`)
+    }
+    else {
+      exec(cmd, (err, stdout) => {
+        if (err) {
+          consola.error(stdout)
+          return
+        }
+        consola.success(stdout)
+      })
+    }
+  }
+  else {
+    consola.info('Canceled')
+  }
+}
+
+async function fillEmpty() {
+  if (type === '') {
+    type = await consola.prompt('What is the commit type? (required)', { type: 'select', options: typeList })
+
+    // check type is clack:cancel
+    if (typeof type === 'symbol')
+      process.exit(0)
+
+    if (!isType(type)) {
+      consola.error(`Invalid commit type: ${type}`, `Commit type must be one of ${typeList.join(', ')}`)
+      process.exit(1)
+    }
+  }
+  if (scope === '' && args.length !== 2)
+    scope = await consola.prompt('What is the commit scope? (optional)', { type: 'text', default: '', placeholder: 'Enter commit scope' })
+  if (body === '')
+    body = await consola.prompt('What is the commit for? (required)', { type: 'text', default: '', placeholder: 'Enter commit body' })
+  if (body === '') {
+    consola.error('Commit body must not be empty')
+    process.exit(1)
+  }
+}
+waitPrompt()
