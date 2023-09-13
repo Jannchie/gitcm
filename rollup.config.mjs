eef19294c46d56594eb4c609295139baa4b93589
Jianqi Pan <jannchie@gmail.com>
2023-09-13 14:57:57 +0900

    :package: build(rollup): expose __filename to global

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -11,7 +11,9 @@ export default {
   output: {
     file: 'lib/index.js',
     format: 'es',
-    banner: '#!/usr/bin/env node',
+    banner: `#!/usr/bin/env node
+import { fileURLToPath } from 'url'
+global['__filename'] = fileURLToPath(import.meta.url)`,
     inlineDynamicImports: true,
   },
   plugins: [
